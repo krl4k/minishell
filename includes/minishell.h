6081e6ea15673fd3fc2592888706eaf41519692a
Magnemite Winter <mwinter@ev-e2.kzn.21-school.ru>
2021-01-25 15:43:21 +0300

    fixed_header

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,5 +1,17 @@
-#ifndef MINISHELL_MINISHELL_H
-# define MINISHELL_MINISHELL_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwinter <mwinter@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/25 15:42:35 by mwinter           #+#    #+#             */
+/*   Updated: 2021/01/25 15:42:50 by mwinter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # include <stdio.h>
 # include <stdlib.h>
