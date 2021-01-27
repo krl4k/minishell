49e1324bbaf08b2a00a090e864820a17ea370867
krl4k <fhds.hdd@mail.ru>
2021-01-27 17:46:18 +0300

    ft_cd

--- srcs/commands/ft_pwd.c
+++ srcs/commands/ft_pwd.c
@@ -1,27 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fgrisell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/27 15:00:41 by fgrisell          #+#    #+#             */
-/*   Updated: 2021/01/27 15:00:42 by fgrisell         ###   ########.fr       */
+/*   Created: 2021/01/27 17:39:05 by fgrisell          #+#    #+#             */
+/*   Updated: 2021/01/27 17:40:40 by fgrisell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int ft_cd(t_all *all)
-{
-	if ((chdir(all->command_argv[1])) == -1)
-	{
-		ft_putstr_fd(PROMT_ERROR, 2);
-		ft_putstr_fd(all->command_argv[0], 2);
-		ft_putstr_fd(": ", 2);
-		ft_putstr_fd(all->command_argv[1], 2);
-		ft_putstr_fd(": ", 2);
-		ft_putendl_fd(strerror(errno), 2);
-	}
-	return (0);
-}
