62ab32bf0e6494043f218ad3617b29a81adfbdfe
krl4k <fhds.hdd@mail.ru>
2021-02-06 20:52:35 +0300

    "ft_execution"

--- srcs/commands/ft_execution.c
+++ srcs/commands/ft_execution.c
@@ -12,6 +12,29 @@
 
 #include "minishell.h"
 
+static int is_pipes(t_all *all)
+{
+	int i;
+	int len;
+
+	len = 0;
+	i = 0;
+	while (all->command_argv[i])
+	{
+		if ((len = ft_strlen(all->command_argv[i])) > 0)
+			if (ft_strcmp(all->command_argv[i], "|") == 0
+				||ft_strcmp(all->command_argv[i], ">") == 0
+				||ft_strcmp(all->command_argv[i], "<") == 0
+				||ft_strcmp(all->command_argv[i], ">>") == 0)
+			{
+				pipes_work(all);
+				return (1);
+			}
+		i++;
+	}
+	return (0);
+}
+
 void ft_execution(t_all *all)
 {
 
@@ -22,46 +45,42 @@ void ft_execution(t_all *all)
 	{
 		printf("command[%d] = %s\n", j, all->command_argv[j]);
 	}
-	while (all->command_argv[i])
-	{
-//		printf("%s\n", all->command_argv[i]);
-		int len = 0;
-		if ((len = ft_strlen(all->command_argv[i])) > 0)
-			if (ft_strcmp(all->command_argv[i], "|") == 0
-				||ft_strcmp(all->command_argv[i], ">") == 0
-				||ft_strcmp(all->command_argv[i], "<") == 0
-				||ft_strcmp(all->command_argv[i], ">>") == 0)
-			{
-				pipes_work(all);
-				return;
-			}
-		i++;
-	}
-
+	if (is_pipes(all))
+		return;
+//	while (all->command_argv[i])
+//	{
+//		int len = 0;
+//		if ((len = ft_strlen(all->command_argv[i])) > 0)
+//			if (ft_strcmp(all->command_argv[i], "|") == 0
+//				||ft_strcmp(all->command_argv[i], ">") == 0
+//				||ft_strcmp(all->command_argv[i], "<") == 0
+//				||ft_strcmp(all->command_argv[i], ">>") == 0)
+//			{
+//				pipes_work(all);
+//				return;
+//			}
+//		i++;
+//	}
 //	for (int j = 0; all->command_argv[j]; ++j)
 //	{
 //		printf("exec cmd[%d] = %s\n", j, all->command_argv[j]);
 //	}
 	if (!all->command_argv[0])
 		return;
-	if (!ft_strncmp(all->command_argv[0], "echo", 4))
+	if (!ft_strcmp(all->command_argv[0], "echo"))
 		ft_echo(all->command_argv);
-	else if (!ft_strncmp(all->command_argv[0], "pwd", ft_strlen(all->command_argv[0])))
+	else if (!ft_strcmp(all->command_argv[0], "pwd"))
 		ft_pwd(all->command_argv);
-	else if (!ft_strncmp(all->command_argv[0], "exit", ft_strlen(all->command_argv[0])))
+	else if (!ft_strcmp(all->command_argv[0], "exit"))
 		ft_exit(all->command_argv);
-	else if (!ft_strncmp(all->command_argv[0], "cd", ft_strlen(all->command_argv[0])))
+	else if (!ft_strcmp(all->command_argv[0], "cd"))
 		ft_cd(all);
-	else if (!ft_strncmp(all->command_argv[0], "env", ft_strlen(all->command_argv[0])))
+	else if (!ft_strcmp(all->command_argv[0], "env"))
 		ft_env(all);
-	else if (!ft_strncmp(all->command_argv[0], "unset", ft_strlen(all->command_argv[0])))
+	else if (!ft_strcmp(all->command_argv[0], "unset"))
 		ft_unset(all);
-	else if (!ft_strncmp(all->command_argv[0], "export", ft_strlen(all->command_argv[0])))
+	else if (!ft_strcmp(all->command_argv[0], "export"))
 		ft_export(all);
 	else
-	{
-
 		execute(all);
-	}
-//	ft_free_split(all->command_argv);
 }
