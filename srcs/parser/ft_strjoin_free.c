aa06f279b1b83269fa393b995008fd3d7390003b
atrumksar@gmail.com <HHGQLF30t>
2021-02-12 19:30:15 +0300

    fixed line parsing

---
+++ srcs/parser/ft_strjoin_free.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwinter <mwinter@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/25 17:59:06 by mwinter           #+#    #+#             */
+/*   Updated: 2021/01/26 13:39:09 by mwinter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char *ft_strjoin_free(char *s1, char *s2)
+{
+	char *res;
+
+	if (!s1 || !s2)
+		return (NULL);
+	res = ft_strjoin(s1, s2);
+	if (s1)
+		free(s1);
+	if (s2)
+		free(s2);
+	if (!res)
+		return (NULL);
+	return (res);
+}
---
+++ srcs/parser/ft_strjoinchar.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwinter <mwinter@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/25 17:59:06 by mwinter           #+#    #+#             */
+/*   Updated: 2021/01/26 13:39:09 by mwinter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_strjoinchar(char *str, char c)
+{
+	char	*new;
+	int		i;
+
+	if (!str || !c)
+		return (NULL);
+	if (!(new = (char *)malloc(sizeof(char) * (ft_strlen(str) + 2))))
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		new[i] = str[i];
+		i++;
+	}
+	free(str);
+	new[i] = c;
+	new[i + 1] = '\0';
+	return (new);
+}
+
